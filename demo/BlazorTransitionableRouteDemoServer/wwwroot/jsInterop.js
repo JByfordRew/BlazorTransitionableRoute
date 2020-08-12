265b33e485af62e8a7b66d38379083b2c320cbcb
JByfordRew <jeremybyfordrew@gmail.com>
2020-08-12 16:02:29 +0100

    add setTimeout to handle browser back and forward actions, otherwise no animation.

--- demo/BlazorTransitionableRouteDemoServer/wwwroot/jsInterop.js
+++ demo/BlazorTransitionableRouteDemoServer/wwwroot/jsInterop.js
@@ -1,26 +1,28 @@
 ﻿window.yourJsInterop = {
     transitionFunction: function (back) {
 
-        let transitionIn = document.getElementsByClassName('transition-in')[0];
-        let transitionOut = document.getElementsByClassName('transition-out')[0];
+        setTimeout(() => {
+            let transitionIn = document.getElementsByClassName('transition-in')[0];
+            let transitionOut = document.getElementsByClassName('transition-out')[0];
 
-        let direction = back ? "Up" : "Down";
+            let direction = back ? "Up" : "Down";
 
-        if (transitionIn && transitionOut) {
+            if (transitionIn && transitionOut) {
 
-            transitionOut.classList.remove('transition-out');
-            transitionOut.classList.add(
-                "animate__fadeOut" + direction,
-                "animate__faster",
-                "animate__animated"
-            );
+                transitionOut.classList.remove('transition-out');
+                transitionOut.classList.add(
+                    "animate__fadeOut" + direction,
+                    "animate__faster",
+                    "animate__animated"
+                );
 
-            transitionIn.classList.remove('transition-in');
-            transitionIn.classList.add(
-                "animate__fadeIn" + direction,
-                "animate__faster",
-                "animate__animated"
-            );
-        }
+                transitionIn.classList.remove('transition-in');
+                transitionIn.classList.add(
+                    "animate__fadeIn" + direction,
+                    "animate__faster",
+                    "animate__animated"
+                );
+            }
+        }, 0);
     }
 }
