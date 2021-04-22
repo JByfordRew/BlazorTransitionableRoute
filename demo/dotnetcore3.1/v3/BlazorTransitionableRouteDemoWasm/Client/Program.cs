3dd2035ca6a34d569b9e050ca8fd0df3902e4d59
JByfordRew <jeremybyfordrew@gmail.com>
2021-04-22 13:24:59 +0100

    v3 initial copy of v2 before v3 changes

---
+++ demo/dotnetcore3.1/v3/BlazorTransitionableRouteDemoWasm/Client/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using BlazorTransitionableRouteDemoWasm.Client.Shared;
+using BlazorTransitionableRoute;
+
+namespace BlazorTransitionableRouteDemoWasm.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSingleton<IRouteTransitionInvoker, DefaultRouteTransitionInvoker>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
