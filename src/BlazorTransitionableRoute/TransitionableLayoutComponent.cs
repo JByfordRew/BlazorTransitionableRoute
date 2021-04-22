760a7d2e1a2ac9f276e6c11a1a8256f703627748
JByfordRew <jeremybyfordrew@gmail.com>
2021-04-22 13:44:55 +0100

    Implemented TransitionType to enable blazor implemented custom transitions

--- src/BlazorTransitionableRoute/TransitionableLayoutComponent.cs
+++ src/BlazorTransitionableRoute/TransitionableLayoutComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System;
 
 namespace BlazorTransitionableRoute
 {
@@ -6,5 +7,9 @@ namespace BlazorTransitionableRoute
     {
         [CascadingParameter]
         public Transition Transition{ get; set; }
+
+        protected (Type fromType, Type toType) TransitionType =>
+            (Transition.IntoView ? Transition.SwitchedRouteData.PageType : Transition.RouteData.PageType,
+             Transition.IntoView ? Transition.RouteData.PageType : Transition.SwitchedRouteData.PageType);
     }
 }
