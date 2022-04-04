323a4a5bf4694a37927fcbb7f0068e0311b8bf3f
JByfordRew <jeremybyfordrew@gmail.com>
2022-04-04 13:57:27 +0100

    .NET 6 Blazor WebAssembly App new project

---
+++ demo/aspnetcore6.0/v4/BlazorTransitionableRouteDemoWasm/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorTransitionableRouteDemoWasm.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorTransitionableRouteDemoWasm.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ demo/aspnetcore6.0/v4/BlazorTransitionableRouteDemoWasm/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorTransitionableRouteDemoWasm.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
